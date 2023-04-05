4a8a90e59189d9d3335047015c775e91a7ea2cd8
ffadhl <fadhlhafizh18@students.unnes.ac.id>
2023-04-05 20:02:11 +0700

    Upload rpl-projects

---
+++ rpl-projects/app/Http/Requests/UpdateKaryawanRequest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateKaryawanRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'txtnama' => 'required',
+            'txttempatlahir' => 'required',
+            'tl' => 'required',
+            'txtjenis_kelamin' => 'required',
+            'txttelp' => 'required',
+            'txtagama' => 'required',
+            'txtpend' => 'required',
+            'txtlisensi' => 'required',
+        ];
+    }
+    public function messages(): array
+    {
+        return [
+            'body.unique' => ':attribute Sudah ada di dalam tabel',
+            'txtnama.required' => ':attribute Tidak Boleh Kosong',
+            'txttempatlahir.required' => ':attribute Tidak Boleh Kosong',
+            'tl.required' => ':attribute Tidak Boleh Kosong',
+            'txtjenis_kelamin.required' => ':attribute Tidak Boleh Kosong',
+            'txttelp.required' => ':attribute Tidak Boleh Kosong',
+            'txtagama.required' => ':attribute Tidak Boleh Kosong',
+            'txtpend.required' => ':attribute Tidak Boleh Kosong',
+            'txtlisensi.required' => ':attribute Tidak Boleh Kosong',
+
+        ];
+    }
+    public function attributes(): array
+    {
+        return [
+            'txtnama' => 'Nama',
+            'txttempatlahir' => 'Tempat Lahir',
+            'tl' => 'Tanggal Lahir',
+            'txtjenis_kelamin' => 'Jenis Kelamin',
+            'txttelp' => 'No Telepon',
+            'txtagama' => 'Agama',
+            'txtpend' => 'Pendidikan',
+            'txtlisensi' => 'Lisensi',
+        ];
+    }
+}
---
+++ rpl-projects/app/Models/Karyawan.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Karyawan extends Model
+{
+    use HasFactory;
+    protected $table = 'karyawan';
+    protected $primaryKey = 'nip';
+
+    public $incrementing = false;
+    public $timestamps = true;
+}
