6697652941feec37fc2683d5e2c44f7aee36bf14
Imam Baihaqi <imambaihaq@students.unnes.ac.id>
2023-04-08 13:52:18 +0700

    Edit Controller

--- app/Http/Controllers/DepartemenController.php
+++ app/Http/Controllers/DepartemenController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Departemen;
-use App\Http\Requests\StoreKaryawanRequest;
-use App\Http\Requests\UpdateKaryawanRequest;
+use App\Http\Requests\StoreDepartemenRequest;
+use App\Http\Requests\UpdateDepartemenRequest;
 
 class DepartemenController extends Controller
 {
@@ -13,7 +13,7 @@ class DepartemenController extends Controller
      */
     public function index()
     {
-        return view('Departemen.data')->with([
+        return view('departemen.data')->with([
             'departemen' => Departemen::all()
         ]);
     }
