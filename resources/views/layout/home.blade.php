1f941350ffd9cba77cf1793e6e4df0d6802f8609
ffadhl <fadhlhafizh18@students.unnes.ac.id>
2023-04-09 18:12:59 +0700

    Coba edit layout

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -51,4 +51,27 @@
             </div>
         </section>
     </div>
+    <div>
+        <section class="py-3 mt-3">
+            <div class="container py-4 py-xl-5">
+                <div class="row gy-4 gy-md-0">
+                    <div
+                        class="col-md-6 text-center text-md-start d-flex d-sm-flex d-md-flex justify-content-center align-items-center justify-content-md-start align-items-md-center justify-content-xl-center">
+                        <div style="max-width: 350px;">
+                            <h1 class="display-6 fw-bold mb-4">Selamat Datang di Pelayanan edit <span
+                                    class="colortext">Data Departemen Depatemen</span>.</h1>
+                            <p class="my-4">Pada Menu Ini Merupakan CRUD Untuk Mengelola Data deaprtemen, Silahkan click
+                                button dibawah untuk Mengelola Data Departemen</p><a class="btn btn-primary btn-lg me-2"
+                                role="button" href="/karyawan">Data Departemen</a>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div><img class="rounded img-fluid w-55 fit-cover" style="min-height: 200px;"
+                                src="{{ asset('illustrasi.png') }}" /></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
 @endsection
