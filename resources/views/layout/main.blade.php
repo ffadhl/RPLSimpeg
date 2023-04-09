d5835a0d1065091b194207611905ef5827441db5
ffadhl <fadhlhafizh18@students.unnes.ac.id>
2023-04-09 12:55:30 +0700

    Update Layout #2

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><img src="{{ asset('logo.png') }}" width="auto" height="60" alt="">Sistem Kepegawaian Rosati</title>
+    <title>Sistem Kepegawaian Rosati</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
