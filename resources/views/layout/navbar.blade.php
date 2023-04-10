34832ea740c032b255d0b62730a607ef4b471349
ffadhl <fadhlhafizh18@students.unnes.ac.id>
2023-04-10 13:53:23 +0700

    update navbar again

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -24,7 +24,7 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
                         data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown link
+                        <b>Office</b>
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                         <li><a class="nav-link {{ request()->segment('1') == 'departemen' ? 'active' : '' }} "
