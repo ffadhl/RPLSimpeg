7bcd3ce320922355cd01b29d9abb68af74455612
Zireha <cheva.wahyu@gmail.com>
2023-04-11 06:31:31 +0700

    unfinished @ controller->show & controller->delete

---
+++ database/migrations/2023_04_10_180116_create_jabatan_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jabatan', function (Blueprint $table) {
+            
+            $table->string('id_jabatan');
+            $table->string('nama_jabatan');
+            $table->string('deskripsi_jabatan');
+            $table->string('lokasi_jabatan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jabatan');
+    }
+};
