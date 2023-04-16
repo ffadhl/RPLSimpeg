ac661b78144cba61d9a0b70eb077539944436047
ffadhl <fadhlhafizh18@students.unnes.ac.id>
2023-04-16 21:59:22 +0700

    controller jabatan

--- routes/web.php
+++ routes/web.php
@@ -65,6 +65,7 @@ Route::resource('penggajian', PenggajianController::class)->middleware('isLogin'
 Route::resource('cuti', CutiController::class)->middleware('isLogin');
 Route::resource('penjadwalan', PenjadwalanController::class)->middleware('isLogin');
 Route::resource('datakeluarga', DataKeluargaController::class)->middleware('isLogin');
+Route::resource('jabatan', JabatanController::class)->middleware('isLogin');
 
 Route::get('/sesi', [SessionController::class, 'index']);
 Route::post('/sesi/login', [SessionController::class, 'login']);
