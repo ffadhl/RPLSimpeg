faa7b378806f3b9abdfdf1af91d54b8f77d2065d
ffadhl <fadhlhafizh18@students.unnes.ac.id>
2023-04-17 23:38:04 +0700

    sidebar

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -122,7 +122,12 @@
     </section>
     <!-- CONTENT -->
 
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
     <script src="{{ asset('js/test.js') }}"></script>
 </body>
 
