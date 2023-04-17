d4fee1baf2331fb77a4edcdb80049d691d49f713
Imam Baihaqi <imambaihaq@students.unnes.ac.id>
2023-04-18 06:41:02 +0700

    tabel presensi

---
+++ app/Models/Presensi.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Presensi extends Model
+{
+    use HasFactory;
+    protected $table = 'presensi';
+    protected $primaryKey = 'id_presensi';
+
+    public $incrementing = false;
+    public $timestamps = true;
+
+}
