f5f8944c8bc1556c13aa7d769bfa81ad27b537d6
Fadhl Al-Hafizh <fadhlhafizh18@students.unnes.ac.id>
2023-05-01 20:11:12 +0700

    commit navbar

---
+++ resources/views/layoutkaryawan/navbar.blade.php
@@ -0,0 +1,95 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('home') }}"><img src="{{ asset('logo.png') }}" width="auto" height="60"
+                alt=""> </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav left">
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->segment('1') == '' || request()->segment('1') == 'home' ? 'active' : '' }}"
+                        aria-current="page" href="{{ url('home') }}">
+                        <i class="fas fa-house"></i> <b>Home</b>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->segment('1') == 'karyawan' ? 'active' : '' }} " aria-current="page"
+                        href="{{ url('karyawan') }}">
+                        <i class="fas fa-user"></i> <b>Karyawan</b>
+                    </a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fa-solid fa-building"></i> <b> Office</b>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="nav-link {{ request()->segment('1') == 'departemen' ? 'active' : '' }} "
+                                aria-current="page" href="{{ url('jabatan') }}">
+                                <i class="fa-solid fa-building-circle-check"></i> <b>Jabatan</b>
+                            </a>
+                        </li>
+                        <li><a class="nav-link {{ request()->segment('1') == 'departemen' ? 'active' : '' }} "
+                                aria-current="page" href="{{ url('departemen') }}">
+                                <i class="fas fa-hospital-alt"></i> <b>Departemen</b>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fa-solid fa-calendar-days"></i> <b> Jadwal</b>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="nav-link {{ request()->segment('1') == 'penjadwalan' ? 'active' : '' }} "
+                                aria-current="page" href="{{ url('penjadwalan') }}">
+                                <i class="fa-regular fa-calendar"></i> <b>Jadwal Karyawan</b>
+                            </a>
+                        </li>
+                        <li><a class="nav-link {{ request()->segment('1') == 'cuti' ? 'active' : '' }} "
+                                aria-current="page" href="{{ url('cuti') }}">
+                                <i class="fas fa-calendar-alt"></i> <b>Cuti</b>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->segment('1') == 'penggajian' ? 'active' : '' }} "
+                        aria-current="page" href="{{ url('presensi') }}">
+                        <i class="fa-solid fa-person-circle-check"></i> <b>Presensi</b>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->segment('1') == 'penggajian' ? 'active' : '' }} "
+                        aria-current="page" href="{{ url('penggajian') }}">
+                        <i class="fab fa-cc-visa"></i> <b>Penggajian</b>
+                    </a>
+                </li>
+
+            </ul>
+            <ul class="navbar-nav right">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false"><span
+                            class="d-none d-lg-inline me-2 text-gray-600 small"><b>{{ Auth::user()->name }}</b></span><img
+                            class="border rounded-circle img-profile" src={{ asset('profile.jpg') }} height="25px"
+                            width="25px" />
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->segment('1') == 'logout' ? 'active' : '' }} "
+                                aria-current="page" href="{{ url('/sesi/logout') }}">
+                                <i class="fa-solid fa-right-from-bracket"></i> <b>Logout</b>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
