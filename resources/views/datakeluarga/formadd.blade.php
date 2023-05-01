0e09e18d77ed5a05fada946d0a5e345c164dcd8f
Imam Baihaqi <imambaihaq@students.unnes.ac.id>
2023-05-01 12:18:20 +0700

    edit radio button

--- resources/views/datakeluarga/formadd.blade.php
+++ resources/views/datakeluarga/formadd.blade.php
@@ -63,17 +63,19 @@
                 <div class="row mb-3">
                     <label for="txtjeniskelamin" class="col-sm-2 col-form-label">Jenis Kelamin</label>
                     <div class="col-sm-4">
-                        <select class="form-select form-select-sm @error('txtjeniskelamin') is-invalid @enderror"
-                            name="txtjeniskelamin" id="txtjeniskelamin">
-                            @error('txtjeniskelamin')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                            @enderror
-                            <option value=" "selected>-pilih-</option>
-                            <option value="LakiLaki">Laki-Laki</option>
-                            <option value="Perempuan">Perempuan</option>
-                        </select>
+                        <div class="form-check">
+                            <input class="form-check-input @error('txtjeniskelamin') is-invalid @enderror" type="radio" name="txtjeniskelamin" id="txtjeniskelamin" value="L">
+                            <label class="form-check-label" for="txtjenis_kelamin">Laki-Laki</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input @error('txtjeniskelamin') is-invalid @enderror" type="radio" name="txtjeniskelamin" id="txtjeniskelamin" value="P">
+                            <label class="form-check-label" for="txtjeniskelamin">Perempuan</label>
+                        </div>
+                        @error('txtjeniskelamin')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                 </div>
                 <div class="row mb-3">
