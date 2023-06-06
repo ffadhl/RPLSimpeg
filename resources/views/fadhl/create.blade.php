75554f94ad4834e76b1babc511919d6acaf39b32
Fadhl Al-Hafizh <fadhlhafizh18@students.unnes.ac.id>
2023-06-06 12:22:52 +0700

    Layout Fadhl

---
+++ resources/views/fadhl/create.blade.php
@@ -0,0 +1,35 @@
+<h1>Data Mahasiswa Baru</h1>
+
+<form action="/mahasiswa/store" method="POST">
+    @csrf
+    <input type="text" name="IDmahasiswa" placeholder="Masukkan ID Mahasiswa"><br>
+    <input type="text" name="nama" placeholder="Masukkan Nama"><br>
+    <select name="jenis_kelamin" id="">
+        <option value="">== Pilih jenis Kelamin ==</option>
+        <option value="L">Laki-laki</option>
+        <option value="P">Perempuan</option>
+    </select><br>
+
+    <select name="prodi" >
+        <option value="">Pilih prodi</option>
+        <option value="Teknik informatika">Teknik informatika</option>
+        <option value="Sistem informasi">Sistem informasi</option>
+    </select><br>
+    
+    <select name="agama" id=" ">
+        <option value="">== Pilih Agama ==</option>
+        <option value="Islam">Islam</option>
+        <option value="Kristen">Kristen</option>
+        <option value="Hindu">Hindu</option>
+        <option value="Budha">Budha</option>
+        <option value="Konghucu">Konghucu</option>
+    </select><br>
+
+    <input type="number" name="NIK" placeholder="Masukkan NIK"><br>
+    <input type="number" name="tlp" placeholder="Masukkan Telepon"><br>
+    <textarea name="alamat" id="" cols="30" rows="10" placeholder="Masukkan Alamat"></textarea><br>
+    <input type="date" name="tanggal_lahir" placeholder="Masukkan Tanggal Lahir"><br>
+    <input type="email" name="email" placeholder="Masukkan Email"><br>
+    <input type="submit" value="Simpan">
+
+</form>
---
+++ resources/views/fadhl/edit.blade.php
@@ -0,0 +1,36 @@
+<h1>Edit Mahasiswa Baru</h1>
+
+<form action="/mahasiswa/{{ $mahasiswa -> NIM }}" method="POST">
+    @method('PUT')
+    @csrf
+    <input type="text" name="IDmahasiswa" placeholder="Masukkan ID Mahasiswa" value="{{ $mahasiswa -> IDmahasiswa }}"><br>
+    <input type="text" name="nama" placeholder="Masukkan Nama" value="{{ $mahasiswa -> nama }}"><br>
+    <select name="jenis_kelamin" id="">
+        <option value="">== Pilih jenis Kelamin ==</option>
+        <option value="L">Laki-laki</option>
+        <option value="P">Perempuan</option>
+    </select><br>
+
+    <select name="prodi" >
+        <option value="">Pilih prodi</option>
+        <option value="Teknik informatika">Teknik informatika</option>
+        <option value="Sistem informasi">Sistem informasi</option>
+    </select><br>
+    
+    <select name="agama" id=" ">
+        <option value="">== Pilih Agama ==</option>
+        <option value="Islam">Islam</option>
+        <option value="Kristen">Kristen</option>
+        <option value="Hindu">Hindu</option>
+        <option value="Budha">Budha</option>
+        <option value="Konghucu">Konghucu</option>
+    </select><br>
+
+    <input type="number" name="NIK" placeholder="Masukkan NIK" value="{{ $mahasiswa -> nik }}"><br>
+    <input type="number" name="tlp" placeholder="Masukkan Telepon" value="{{ $mahasiswa -> tlp }}"><br>
+    <textarea name="alamat" id="" cols="30" rows="10" placeholder="Masukkan Alamat"></textarea><br>
+    <input type="date" name="tanggal_lahir" placeholder="Masukkan Tanggal Lahir" value="{{ $mahasiswa -> tanggal_lahir }}"><br>
+    <input type="email" name="email" placeholder="Masukkan Email" value="{{ $mahasiswa -> email }}"><br>
+    <input type="submit" value="Simpan">
+
+</form>
