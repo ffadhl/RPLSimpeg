49929d777dc6c0e0b53a02639ee067a4b4258483
Zireha <cheva.wahyU@gmail.com>
2023-06-06 13:38:12 +0700

    "added uas cheva crud"

---
+++ database/migrations/2023_06_06_033900_mahasiswacheva.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswachevas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->integer('nik');
+            $table->string('jenis_kelamin');
+            $table->string('prodi');
+            $table->string('agama');
+            $table->integer('telepon');
+            $table->string('alamat');
+            // $table->timestamps();
+            // $table->string('email', 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
