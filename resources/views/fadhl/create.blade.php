afa8a5e2e31ab12415a186e79bf8b9256f61651d
Fadhl Al-Hafizh <fadhlhafizh18@students.unnes.ac.id>
2023-06-09 18:28:58 +0700

    layout

--- resources/views/fadhl/create.blade.php
+++ resources/views/fadhl/create.blade.php
@@ -27,27 +27,48 @@
                     </select><br>
                 </div>
 
-                <select name="prodi">
-                    <option value="">Pilih prodi</option>
-                    <option value="Teknik informatika">Teknik informatika</option>
-                    <option value="Sistem informasi">Sistem informasi</option>
-                </select><br>
+                <div class="row mb-3">
+                    <select name="prodi">
+                        <option value="">Pilih prodi</option>
+                        <option value="Teknik informatika">Teknik informatika</option>
+                        <option value="Sistem informasi">Sistem informasi</option>
+                    </select><br>
+                </div>
 
-                <select name="agama" id=" ">
-                    <option value="">== Pilih Agama ==</option>
-                    <option value="Islam">Islam</option>
-                    <option value="Kristen">Kristen</option>
-                    <option value="Hindu">Hindu</option>
-                    <option value="Budha">Budha</option>
-                    <option value="Konghucu">Konghucu</option>
-                </select><br>
+                <div class="row mb-3">
+                    <select name="agama" id=" ">
+                        <option value="">== Pilih Agama ==</option>
+                        <option value="Islam">Islam</option>
+                        <option value="Kristen">Kristen</option>
+                        <option value="Hindu">Hindu</option>
+                        <option value="Budha">Budha</option>
+                        <option value="Konghucu">Konghucu</option>
+                    </select><br>
+                </div>
 
-                <input type="number" name="NIK" placeholder="Masukkan NIK"><br>
-                <input type="number" name="tlp" placeholder="Masukkan Telepon"><br>
-                <textarea name="alamat" id="" cols="30" rows="10" placeholder="Masukkan Alamat"></textarea><br>
-                <input type="date" name="tanggal_lahir" placeholder="Masukkan Tanggal Lahir"><br>
-                <input type="email" name="email" placeholder="Masukkan Email"><br>
-                <input type="submit" value="Simpan">
+                <div class="row mb-3">
+                    <input type="number" name="NIK" placeholder="Masukkan NIK"><br>
+                </div>
+
+                <div class="row mb-3">
+                    <input type="number" name="tlp" placeholder="Masukkan Telepon"><br>
+                </div>
+
+                <div class="row mb-3">
+                    <textarea name="alamat" id="" cols="30" rows="10" placeholder="Masukkan Alamat"></textarea><br>
+                </div>
+
+                <div class="row mb-3">
+                    <input type="date" name="tanggal_lahir" placeholder="Masukkan Tanggal Lahir"><br>
+                </div>
+
+                <div class="row mb-3">
+                    <input type="email" name="email" placeholder="Masukkan Email"><br>
+                </div>
+
+                <div class="row mb-3">
+                    <input type="submit" value="Simpan">
+                </div>
             </form>
         </div>
     </div>
